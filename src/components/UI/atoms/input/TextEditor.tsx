6c207539aee417985087d888ae7e0d3f65c7af95
christianlarry <christianlarry03@gmail.com>
2024-10-21 11:54:13 +0700

    Added text TextEditor

--- src/components/UI/atoms/input/TextEditor.tsx
+++ src/components/UI/atoms/input/TextEditor.tsx
@@ -1,10 +1,65 @@
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import {
+  ClassicEditor,
+  Bold,
+  Essentials,
+  Heading,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  List,
+  MediaEmbed,
+  Paragraph,
+  Table,
+  Undo
+} from 'ckeditor5';
+
+import 'ckeditor5/ckeditor5.css';
+
 import "./TextEditor.css"
 
-const TextEditor = ()=>{
+interface Props{
+  dataState:[string,React.Dispatch<React.SetStateAction<string>>]
+}
+
+const TextEditor = ({dataState}:Props)=>{
+
+  const [editorData, setEditorData] = dataState
 
   return (
-    <div>
-      <h1>Editor</h1>
+    <div className='text-editor'>
+      <CKEditor
+        editor={ ClassicEditor }
+        config={ {
+          toolbar: [
+            'undo', 'redo', '|',
+            'heading', '|', 'bold', 'italic', '|',
+            'link', 'insertTable', 'mediaEmbed', '|',
+            'bulletedList', 'numberedList', 'indent', 'outdent'
+          ],
+          plugins: [
+            Bold,
+            Essentials,
+            Heading,
+            Indent,
+            IndentBlock,
+            Italic,
+            Link,
+            List,
+            MediaEmbed,
+            Paragraph,
+            Table,
+            Undo
+          ],
+          initialData: "",
+        } }
+        data={editorData}
+        onChange={(_e,editor)=>{
+          const data = editor.getData()
+          setEditorData(data)
+        }}
+      />
     </div>
   )
 }
